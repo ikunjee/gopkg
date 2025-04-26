249d57c9e738e60c3138754139beb6ed6ab5b909
ikunjee <Hui4401@qq.com>
2025-04-26 20:31:13 +0800

    logx

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	// 默认 product 级别只打印 info 及以上日志，不输出到文件，不启用 color，可通过 InitDefaultLogger 自定义设置
-	logx.InitDefaultLogger(logx.LogModeDevelopment, "logx/out", true)
+	logx.InitDefaultLogger(logx.LogModeDevelopment, "logx/output", true)
 	logx.Info("hello info")
 	logx.Error("hello error")
 }
